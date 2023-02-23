664073799de21e73b9e5af4dbfec92e82e6d223d
Steve <stevedsimkins@gmail.com>
2023-02-23 10:54:55 -0500

    done

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: 'https://my-blog-site.netlify.app',
+    site: 'https://astro-blog-tutorial-rho.vercel.app/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
